f7369f0bbf99cebb3588dbda2b6138cc311ce049
INFLEXION\sanjay <sanjay.p@inflexiontech.com>
2017-10-30 16:18:04 +0530

    copspace object created in jsonhelper

--- helpers/json-helper.js
+++ helpers/json-helper.js
@@ -2,6 +2,50 @@
 "esversion:6";
 "strict mode";
 
+
+module.exports.getcoSpaceObject = (data)=>{
+    return new Promise((resolve, reject) => {
+        let coSpaceObj = "name="+data.coSpace + "uri="+data.uri + "passcode=" + data.passcode;
+
+
+        if(typeof data.secondaryUri != 'undefined' && data.secondaryUri != null)
+            coSpaceObj += "secondaryUri"+data.secondaryUri;
+        if(typeof data.callId != 'undefined' && data.callId != null)
+             coSpaceObj += "callId"+data.callId;
+        if(typeof data.cdrTag != 'undefined' && data.cdrTag != null)
+            coSpaceObj += "cdrTag"+data.cdrTag;
+        if(typeof data.defaultLayout != 'undefined' && data.defaultLayout != null)
+            coSpaceObj += "defaultLayout"+data.defaultLayout;
+        if(typeof data.tenant != 'undefined' && data.tenant != null)
+            coSpaceObj += "tenant"+data.tenant;
+        if(typeof data.callLegProfile != 'undefined' && data.callLegProfile != null)
+            coSpaceObj += "callLegProfile"+data.callLegProfile;
+        if(typeof data.callProfile != 'undefined' && data.callProfile != null)
+            coSpaceObj += "callProfile"+data.callProfile;
+        if(typeof data.callBrandingProfile != 'undefined' && data.callBrandingProfile != null)
+            coSpaceObj += "callBrandingProfile"+data.callBrandingProfile;
+        if(typeof data.requireCallId != 'undefined' && data.requireCallId != null)
+            coSpaceObj += "requireCallId"+data.requireCallId;
+        if(typeof data.secret != 'undefined' && data.secret != null)
+            coSpaceObj += "secret"+data.secret;
+        if(typeof data.regenerateSecret != 'undefined' && data.regenerateSecret != null)
+            coSpaceObj += "regenerateSecret"+data.regenerateSecret;
+        if(typeof data.nonMemberAccess != 'undefined' && data.nonMemberAccess != null)
+            coSpaceObj += "nonMemberAccess"+data.nonMemberAccess;
+        if(typeof data.ownerJid != 'undefined' && data.ownerJid != null)
+            coSpaceObj += "ownerJid"+data.ownerJid;
+        if(typeof data.streamUrl != 'undefined' && data.streamUrl != null)
+             coSpaceObj += "streamUrl"+data.streamUrl;
+        if(typeof data.ownerAdGuid != 'undefined' && data.ownerAdGuid != null)
+            coSpaceObj += "ownerAdGuid"+data.ownerAdGuid;
+        if(typeof data.meetingScheduler != 'undefined' && data.meetingScheduler != null)
+            coSpaceObj += "meetingScheduler"+data.meetingScheduler;
+       
+        
+        resolve(coSpaceObj);
+    })
+}
+
 module.exports.getMeetingObject = (data)=>{
     return new Promise((resolve, reject) => {
         let meetingObj = {
@@ -18,8 +62,6 @@ module.exports.getMeetingObject = (data)=>{
             meetingObj.description = data.description;
         if(typeof data.meetingStatus != 'undefined' && data.meetingStatus != null)
             meetingObj.meetingStatus = data.meetingStatus;
-        if(typeof data.defaultLayout != 'undefined' && data.defaultLayout != null)
-            meetingObj.defaultLayout = data.defaultLayout;
         if(typeof data.defaultLayout != 'undefined' && data.defaultLayout != null)
             meetingObj.defaultLayout = data.defaultLayout;
         if(typeof data.isMeetingCreated != 'undefined' && data.isMeetingCreated != null)
